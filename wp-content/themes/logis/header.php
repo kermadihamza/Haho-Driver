ff9377b8d557a48ad869fbfe73a88dc1fcbacbba
Hamza <kermadih@gmail.com>
2019-05-22 10:24:16 +0200

    test change home en hamza et header analytics

--- wp-content/themes/logis/header.php
+++ wp-content/themes/logis/header.php
@@ -1,4 +1,3 @@
-
     <title>Haho-Driver | Your daily driver</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -22,17 +21,7 @@
 <?php
 wp_head();
 ?>
-    
   </head>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140515867-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-140515867-1');
-</script>
   <body data-spy="scroll" data-target=".site-navbar-target" data-offset="200">
   
   <!-- <div class="site-wrap"> -->
