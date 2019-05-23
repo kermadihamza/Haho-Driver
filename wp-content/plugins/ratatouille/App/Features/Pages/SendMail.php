2cf492781bbc0ddc4862dddceb5928000fb73840
Hamza <kermadih@gmail.com>
2019-05-23 04:12:06 +0200

    test mail

--- wp-content/plugins/ratatouille/App/Features/Pages/SendMail.php
+++ wp-content/plugins/ratatouille/App/Features/Pages/SendMail.php
@@ -18,7 +18,7 @@ class SendMail
       __('Mail'), // le texte dans le menu
       'edit_private_pages', // la capacité qu'il faut posséder en tant qu'utilisateur pour avoir accès à cette page (les roles et capacité seront vue plus tard)
       'mail-client', // Le slug du menu
-      [self::class, 'render'], // La méthode qui va afficher la page
+      [self::class, 'render'], // La méthode qui va afficher la page!
       'dashicons-email-alt', // L'icon dans le menu
       26 // la position dans le menu (à comparer avec la valeur deposition des autres liens menu que l'on retrouve dans la doc).
     );
