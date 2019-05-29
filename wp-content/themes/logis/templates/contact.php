10dbf08e51f0bdfbb049cf4713376e985c7adbfd
Hamza <kermadih@gmail.com>
2019-05-29 16:19:26 +0200

    ajout required pour obliger a tout remplir

--- wp-content/themes/logis/templates/contact.php
+++ wp-content/themes/logis/templates/contact.php
@@ -13,25 +13,25 @@ $button = get_theme_mod('coding-contact-button');
         <h2 class="font-weight-light text-primary">Contact Us</h2>
       </div>
     </div>
+    <?php include plugin_dir_path(__FILE__) . "../includes/notice.php";?>
     <div class="row">
       <div class="col-md-7 mb-5">
         
-  <?php include plugin_dir_path(__FILE__) . "../includes/notice.php";?>
         <form action="<?= admin_url('admin-post.php'); ?>#section-contact" method="post" class="p-5 bg-white">
           <input type="hidden" name="action" value="send-mail">
 
           <div class="row form-group">
             <div class="col-md-6 mb-3 mb-md-0">
               <label class="text-black" for="fname">First Name</label>
-              <input type="text" id="firstname" class="form-control" type="text" name="firstname" id="firstname" value="<?= isset($old['firstname']) ? $old['firstname'] : '' ?>">
+              <input type="text" id="firstname" class="form-control" type="text" name="firstname" id="firstname" value="<?= isset($old['firstname']) ? $old['firstname'] : '' ?> "required>
             </div>
             <div class="col-md-6">
               <label class="text-black" for="lname">Last Name</label>
-              <input type="text" id="name" class="form-control" type="text" name="name" id="name" value="<?= isset($old['name']) ? $old['name'] : '' ?>">
+              <input type="text" id="name" class="form-control" type="text" name="name" id="name" value="<?= isset($old['name']) ? $old['name'] : '' ?> "required>
             </div>
             <div class="col-md-12">
               <label class="text-black" for="lname">Subject</label>
-              <input type="text" id="subject" class="form-control" type="text" name="name" id="name" value="<?= isset($old['subject']) ? $old['subject'] : '' ?>">
+              <input type="text" id="subject" class="form-control" type="text" name="name" id="name" value="<?= isset($old['subject']) ? $old['subject'] : '' ?> "required>
             </div>
           </div>
 
@@ -39,14 +39,14 @@ $button = get_theme_mod('coding-contact-button');
 
             <div class="col-md-12">
               <label class="text-black" for="email">Email</label>
-              <input type="email" id="email" class="form-control" type="email" name="email" id="email" value="<?= isset($old['email']) ? $old['email'] : '' ?>">
+              <input type="email" id="email" class="form-control" type="email" name="email" id="email" value="<?= isset($old['email']) ? $old['email'] : '' ?> "required>
             </div>
           </div>
 
           <div class="row form-group">
             <div class="col-md-12">
               <label class="text-black" for="message">Message</label>
-              <textarea name="message" id="message" cols="30" rows="7" class="form-control" name="message" placeholder="Pour une résérvation, veuillez nous mettre la date, l'heure et les endroits" id="message" cols="30" rows="10"><?= isset($old['message']) ? $old['message'] : '' ?></textarea>
+              <textarea required name="message" id="message" cols="30" rows="7" class="form-control" name="message" placeholder="Pour une résérvation, veuillez nous mettre la date, l'heure et les endroits" id="message" cols="30" rows="10"><?= isset($old['message']) ? $old['message'] : '' ?></textarea>
             </div>
           </div>
           <div class="row form-group">
